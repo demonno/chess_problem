4c6212e30c041af176511d41b905bdd14fd1b088
demonno <demur.nodia@gmail.com>
2016-04-26 20:40:42 +0400

    "add docstring to pieces "

--- peaces/figures.py
+++ peaces/figures.py
@@ -12,7 +12,10 @@ PIECES = (
 
 
 class Piece(object):
+    """Base class for chess pieces"""
+
     def __init__(self, piece_type):
+        """Use for identification of piece"""
         self._piece_type = piece_type
 
     @property
@@ -29,29 +32,28 @@ class Piece(object):
 
     @abstractmethod
     def can_kill_piece(self, current_position, pieces_on_board, m, n):
-        """return True if piece can kill at least one existing piece"""
+        """Check if piece can kill at least one existing piece. return True."""
 
     @abstractmethod
     def can_move_to(self, column, row, m, n):
-        """return list of coordinates where piece can move."""
+        """Find all coordinates where piece can move. return list of tuples."""
+
+    @staticmethod
+    def filter_moves(moves, m, n):
+        """Filter moves values surrounded by 0 and m, n. return filtered list."""
+        return filter(lambda x: 0 < x[0] <= m and 0 < x[1] <= n, moves)
 
 
 class King(Piece):
+    """class `King` chess piece"""
 
     def __init__(self):
-        Piece.__init__(self, 'K')
+        super(King, self).__init__('K')
 
     def can_kill_piece(self, current_position, pieces_on_board, m, n):
-        """
-        return `True` if King from `current_position` can kill at least one piece on board.
-        :param current_position:
-        :param pieces_on_board:
-        :param m:
-        :param n:
-        :return: True or False
-        """
+        """Check if King can kill at least one piece on board from `current_position`. return True."""
         moves = self.can_move_to(current_position[0], current_position[1], m, n)
-        
+
         for piece in pieces_on_board:
             if piece in moves:
                 return True
@@ -59,46 +61,47 @@ class King(Piece):
         return False
 
     def can_move_to(self, column, row, m, n):
-        """
-        Determine all moves, which King can do, from `column` and `row` square
+        """Determine all moves, which King can do, from `column` and `row` square
         parameters `m`, `n` are board information
         according them moves which go out of the board are not included
 
-        :param row:
-        :param column:
+        :param row: initial row of piece
+        :param column: initial column of piece
         :param m: width of the board
         :param n: height of the board
         :return: list of columns and rows where King can move
         """
-        moves = [(column - 1, row - 1),
-                 (column - 1, row - 0),
-                 (column - 1, row + 1),
+        moves = [
+            (column - 1, row - 1),
+            (column - 1, row - 0),
+            (column - 1, row + 1),
 
-                 (column - 0, row - 1),
-                 (column - 0, row + 1),
+            (column - 0, row - 1),
+            (column - 0, row + 1),
 
-                 (column + 1, row - 1),
-                 (column + 1, row - 0),
-                 (column + 1, row + 1),
-                 ]
-        moves = filter(lambda x: 0 < x[0] <= m and 0 < x[1] <= n, moves)
+            (column + 1, row - 1),
+            (column + 1, row - 0),
+            (column + 1, row + 1),
+        ]
+        moves = self.filter_moves(moves, m, n)
         return moves
 
 
 class Rook(Piece):
+    """class `Rook` chess piece"""
 
     def __init__(self):
-        Piece.__init__(self, 'R')
+        super(Rook, self).__init__('R')
 
     def can_kill_piece(self, current_position, pieces_on_board, m, n):
-        """return `True` if Rook from `current_position` can kill at least one piece on board."""
+        """Check if `Rook` can kill at least one piece on board from `current_position`. return True."""
         for piece in pieces_on_board:
             if piece[0] == current_position[0] or piece[1] == current_position[1]:
                 return True
         return False
 
     def can_move_to(self, column, row, m, n):
-        """return list of possible positions where Rook can move from current `column` and `row`."""
+        """Find all possible positions where `Rook` can move from current `column` and `row`. return list of tuples."""
         moves = []
         m += 1
         n += 1
@@ -112,11 +115,13 @@ class Rook(Piece):
 
 
 class Bishop(Piece):
+    """class `Bishop` chess piece"""
 
     def __init__(self):
-        Piece.__init__(self, 'B')
+        super(Bishop, self).__init__('B')
 
     def can_kill_piece(self, current_position, pieces_on_board, m, n):
+        """check if `Bishop` can kill at least one piece on board from `current_position` . return True."""
         moves = self.can_move_to(current_position[0], current_position[1], m, n)
 
         for piece in pieces_on_board:
@@ -126,27 +131,36 @@ class Bishop(Piece):
         return False
 
     def can_move_to(self, column, row, m, n):
+        """Find all possible positions where `Bishop` can move from current `column` and `row`.
+
+        :param column: current column index of piece
+        :param row: current row index of piece
+        :param m: width of the board
+        :param n: height of the board
+        :return: list of tuples.
+        """
         moves = []
         m += 1
         for i in xrange(1, m - column):
-            moves.append((column+i, row+i))
-            moves.append((column+i, row-i))
+            moves.append((column + i, row + i))
+            moves.append((column + i, row - i))
 
         for i in xrange(1, column):
-            moves.append((column-i, row+i))
-            moves.append((column-i, row-i))
+            moves.append((column - i, row + i))
+            moves.append((column - i, row - i))
 
-        # TODO try to reuse it from King Piece or add conditions in if
-        moves = filter(lambda x: 0 < x[0] <= m and 0 < x[1] <= n, moves)
+        moves = self.filter_moves(moves, m, n)
         return moves
 
 
 class Knight(Piece):
+    """class `Knight` chess piece"""
 
     def __init__(self):
-        Piece.__init__(self, 'N')
+        super(Knight, self).__init__('N')
 
     def can_kill_piece(self, current_position, pieces_on_board, m, n):
+        """check if `Knight` can kill at least one piece on board from `current_position`. return True."""
         moves = self.can_move_to(current_position[0], current_position[1], m, n)
 
         for piece in pieces_on_board:
@@ -156,6 +170,14 @@ class Knight(Piece):
         return False
 
     def can_move_to(self, column, row, m, n):
+        """Find all possible positions where `Knight` can move from current `column` and `row
+
+        :param column: current column index of piece
+        :param row: current row index of piece
+        :param m: width of the board
+        :param n: height of the board
+        :return: list of tuples
+        """
         moves = [
             (column + 2, row + 1),
             (column + 2, row - 1),
@@ -167,23 +189,24 @@ class Knight(Piece):
             (column + 1, row - 2),
             (column - 1, row - 2),
         ]
-        # TODO try to reuse it from King Piece
-        moves = filter(lambda x: 0 < x[0] <= m and 0 < x[1] <= n, moves)
+
+        moves = self.filter_moves(moves, m, n)
         return moves
 
 
 class Queen(Rook, Bishop):
+    """class `Queen` chess piece"""
 
     def __init__(self):
         Piece.__init__(self, 'Q')
 
     def can_kill_piece(self, current_position, pieces_on_board, m, n):
+        """check if `Queen` can kill at least one piece on board from `current_position`. return True."""
         return Rook.can_kill_piece(self, current_position, pieces_on_board, m, n) \
-               or Bishop.can_kill_piece(self, current_position, pieces_on_board, m, n)
+            or Bishop.can_kill_piece(self, current_position, pieces_on_board, m, n)
 
     def can_move_to(self, column, row, m, n):
+        """Find all possible positions where `Queen` can move from current `column` and `row`. return list of tuples."""
         moves = Rook.can_move_to(self, column, row, m, n)
         moves += Bishop.can_move_to(self, column, row, m, n)
         return moves
-
-
