f69096156ad4f387d37f651b7e93a01f8765600e
demonno <demur.nodia@gmail.com>
2016-04-29 19:47:25 +0400

    add 7 x 7 problem in example

--- example.py
+++ example.py
@@ -1,6 +1,17 @@
+import time
 from boards.chess_board import ChessBoard
-from pieces.pieces import Rook, King
+from pieces.pieces import Rook, King, Queen, Bishop, Knight
 
-board = ChessBoard(3, 3, [King(), King(), Rook()])
+# board = ChessBoard(3, 3, [King(), King(), Rook()])
+# board.solve()
+# board.print_solutions(include_quantity=True)
+
+# 7 7 board with 2 Kings, 2 Queens, 2 Bishops and 1 Knight.
+# Also provide the time it took to get the final score. Needless to say, the lower the time, the better.
+
+start_time = time.time()
+board = ChessBoard(7, 7, [King(), King(), Queen(), Queen(), Bishop(), Bishop(), Knight()])
 board.solve()
-board.print_solutions(include_quantity=True)
+
+print "Total Number of unique configurations %s " % board.number_of_solutions()
+print "Time for solving problem %s seconds" % (time.time() - start_time)
